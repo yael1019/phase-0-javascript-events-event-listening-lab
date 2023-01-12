30d51045ee353b982daf05bbaf61ad1532003bd6
Yael Harosh <yaelharosh01@gmail.com>
2023-01-12 12:46:38 -0500

    pass events lab

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.querySelector('input#button')
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    input.addEventListener('click', clickAlert);
 }
